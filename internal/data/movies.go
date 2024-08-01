f9ebc0964b75d122db01cb7195782927667ac49b
declanlin <declanl482@gmail.com>
2024-08-01 19:59:37 -0400

    defined model for a movie

---
+++ internal/data/movies.go
@@ -0,0 +1,15 @@
+package data
+
+import "time"
+
+// Annotate the Movie struct with struct tags to control how the keys appear in the
+// JSON-encoded output.
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
+}
