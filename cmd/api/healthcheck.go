683a326f66d6a31fe315f3dbe15c8284e444c4e1
declanlin <declanl482@gmail.com>
2024-08-01 07:34:22 -0400

    refactored healthcheck handler to return JSON response body

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -8,7 +8,20 @@ import (
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Create a fixed JSON format from a string.
+	// Using raw string literals to use double quotes without the need to escape them.
+	// Use the %q format specifier to wrap the interpolated values in double quotes.
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	// Set the "Content-Type: application/json" header on the response.
+	// If you forget this, Go will default to sending a "Content-Type: text/plain; charset=utf-8"
+	// header instead.
+
+	// JSON RFC: JSON text exchanged between systems that are not part of a closed ecosystem MUST be
+	// encoded using UTF-8. (hence why we are not using "application/json; charset=utf-8")
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON to the http.ResponseWriter as the response body.
+	w.Write([]byte(js))
 }
