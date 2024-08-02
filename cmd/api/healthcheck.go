da92459132b95f649c382f92d1ed937fe2d6e53f
declanlin <declanl482@gmail.com>
2024-08-01 20:01:45 -0400

    enveloped healthcheck handler response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -9,31 +8,20 @@ import (
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 
-	// Create a map which holds the information we want to send in the response.
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	// Declare an envelope map containing the data for the response. Notice that the way
+	// we've constructed this means the environment and version data will now be nested
+	// under a system_info key in the JSON response.
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
-	// Pass the map to the json.Marshal() function.
-	// json.Marshal() takes a native Go object and converts it to a byte slice containing the JSON data.
-	js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		http.Error(w, "The server encountered a problem", http.StatusInternalServerError)
 	}
-
-	js = append(js, '\n')
-
-	// Set the "Content-Type: application/json" header on the response.
-	// If you forget this, Go will default to sending a "Content-Type: text/plain; charset=utf-8"
-	// header instead.
-
-	// JSON RFC: JSON text exchanged between systems that are not part of a closed ecosystem MUST be
-	// encoded using UTF-8. (hence why we are not using "application/json; charset=utf-8")
-	w.Header().Set("Content-Type", "application/json")
-
-	// Write the JSON to the http.ResponseWriter as the response body.
-	w.Write(js)
 }
