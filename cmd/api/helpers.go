b1b1cdaa68fea63511788ea9f6135c378bba8d50
declanlin <declanl482@gmail.com>
2024-08-01 20:00:55 -0400

    added helper method for writing an HTTP response (headers+body) for a JSON body

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -21,4 +22,42 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+type envelope map[string]interface{}
+
+// Define a helper for sending HTTP responses.
+// - w: io.Writer to write the response to
+// - status: the HTTP status code to send
+// - data: the data to encode to a JSON
+// - headers: a header map containing additional HTTP headers to include in the response.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	// Use the json.MarshalIndent() function so that whitespace is added to the encoded
+	// JSON. Here we use no line prefix ("") and tab indents ("\t") for each element.
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	// Append a new line to the JSON body to make it easier to read in terminal application outputs.
+	js = append(js, '\n')
+
+	// We won't run into errors before writing the response. It is safe to add any headers that we want to include.
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	// Set the "Content-Type: application/json" header on the response.
+	// If you forget this, Go will default to sending a "Content-Type: text/plain; charset=utf-8"
+	// header instead.
+
+	// JSON RFC: JSON text exchanged between systems that are not part of a closed ecosystem MUST be
+	// encoded using UTF-8. (hence why we are not using "application/json; charset=utf-8")
+	w.Header().Set("Content-Type", "application/json")
+
+	// Send the response headers with the specified status code.
+	w.WriteHeader(status)
+	// Send the JSON response body.
+	w.Write(js)
+	return nil
 }
